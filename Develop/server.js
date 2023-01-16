3f461910413688a1ccfb30c4e0c343c184acdd9c
Mona Lisa <cjensen@netechnical.com>
2023-01-16 12:40:20 -0600

    upadted get requests/responses

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,29 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
+const notesDb = require('./db/db.json');
 
-// Require the JSON file and assign it to a variable
-const notesDb = require('./public/notes.html');
 const PORT = 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+app.use(express.static('public'));
 
-// res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api', (req, res) => res.json(notesDb));
+app.get('/', (req, res) => res.send('Navigate to /notes'));
+
+app.get('/api/notesDb', (req, res) => res.json(notesDb));
+
+app.get('/index', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
 
 
