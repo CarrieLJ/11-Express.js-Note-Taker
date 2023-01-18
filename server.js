522f8736cd558c90b71d75af94512b91b311616c
Mona Lisa <cjensen@netechnical.com>
2023-01-18 14:05:37 -0600

    corrected my folder structure, updated the path for uuid in store.js, and created a uuid.js in the helpers folder

---
+++ server.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const express = require("express");
+// const path = require("path");
+// const dbData = require("./db/db.json");
+// const api = require('./public/assets/js/index.js');
+// const index = require('./public/assets/js/index.js');
+//call out apiroute and htmlroute
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.static("public"));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
