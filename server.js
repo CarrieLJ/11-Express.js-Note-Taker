c5b62ca67a5de5b667502654a2429fcd27647d68
Mona Lisa <cjensen@netechnical.com>
2023-01-25 07:28:22 -0600

    updated order of app.use for express

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static("public"));
-app.use("/api", apiRoute);
-app.use("/", htmlRoute);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRoute);
+app.use("/", htmlRoute);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
